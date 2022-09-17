b3271da455ca3a526434bece94d41187e2bf2a80
Haoyun Lu <auskaoscar@outlook.com>
2022-09-17 15:26:01 +0800

    config the database

--- database/config/admin.js
+++ database/config/admin.js
@@ -1,8 +1,14 @@
 module.exports = ({ env }) => ({
+  // auth: {
+  //   secret: env('ADMIN_JWT_SECRET'),
+  // },
+  // apiToken: {
+  //   salt: env('API_TOKEN_SALT'),
+  // },
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET','LEaKwgAYmoMFuPBrsQ5ZwQ'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT','XCrnCQhOHvyK3cdtyx5ROA'),
   },
 });
--- database/config/server.js
+++ database/config/server.js
@@ -1,7 +1,10 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  // app: {
+  //   keys: env.array('APP_KEYS'),
+  // },
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS',['VXe1XB1jT52gAfDUdrPqKQ', 'B6nd556hI+5sE2VqUAUtqw','N61Tdi0yX7fxmxIsaPWcsw','yzc2/nIczrWHN4szxhF7VA'])
   },
 });
